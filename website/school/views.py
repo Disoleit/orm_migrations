3f484feac0b45fe540a75150b97c411b2a431aa8
Disoleit <sagatl1994@gmail.com>
2025-07-18 12:05:31 +0300

    worked

--- website/school/views.py
+++ website/school/views.py
@@ -6,10 +6,21 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
+    ordering = 'group'
+    students = Student.objects.all().order_by(ordering)
+    context = {'object_list': students}
+    print("\n=== Список учеников с учителями ===")
+    for student in students:
+        print(f"\nУченик: {student.name} ({student.group})")
+        print("Преподаватели:")
+        for teacher in student.teachers.all():
+            print(f"- {teacher.name} ({teacher.subject})")
+        if not student.teachers.exists():
+            print("- Нет преподавателей")
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
+
+    # prefetch_related('teachers')
 
     return render(request, template, context)
